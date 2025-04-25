5e9f5d0dae954280aafa834fa079a6d8bc23609c
nupoo <nuve19ec@cmrit.ac.in>
2025-04-25 14:43:05 +0530

    updates

--- firebase.ts
+++ firebase.ts
@@ -2,6 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth} from "firebase/auth";
 import { getFirestore} from "firebase/firestore";
 
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDfwQJEK9JQ-KAdCH6safbglz_Gq3WQj8A",
